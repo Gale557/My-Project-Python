ec214f22b6f7e1419dbf56023294057d8d2d1e73
USER <christiangale96@gmail.com>
2022-09-08 09:49:11 +0800

    Latest

--- Jumlah_Buku.py
+++
@@ -1,6 +0,0 @@
-jumlah_Buku = 10
-jumlah_Buku_Yang_Sudah_Dibaca = 0
-
-for jumlah_Buku_Yang_Sudah_Dibaca in range(1, jumlah_Buku + 1):
-    print(f"Jumlah buku yg sudah dibaca ada {jumlah_Buku_Yang_Sudah_Dibaca}")
-    jumlah_Buku_Yang_Sudah_Dibaca += 1
---
+++ Name_Input.py
@@ -0,0 +1,7 @@
+def function(fName, lName):
+    print(fName + " " + lName)
+
+
+fName = input(str)
+lName = input(str)
+function(fName, lName)
